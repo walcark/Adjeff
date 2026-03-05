ea828b6f5a9e6a65dd6879ee90c9735a71268775
walcark <kevin.walcarius@cnes.fr>
2026-03-05 15:34:39 +0100

    feat: add a singleton logging class for adjeff.
    - the class instance parameters can be change anywhere in the project.
    - the class instance can return a logger and provides a time decorator.

--- src/adjeff/utils/__init__.py
+++ src/adjeff/utils/__init__.py
@@ -4,6 +4,14 @@ Those utilities are generally writen to simplify usage of third-party packages.
 """
 
 from .convolve import fft_convolve_2D, fft_convolve_2D_torch
+from .logger import LoggerConfig, adjeff_logging
 from .xrutils import grid, square_grid
 
-__all__ = ["fft_convolve_2D", "fft_convolve_2D_torch", "square_grid", "grid"]
+__all__ = [
+    "fft_convolve_2D",
+    "fft_convolve_2D_torch",
+    "adjeff_logging",
+    "LoggerConfig",
+    "square_grid",
+    "grid",
+]
