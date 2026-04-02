1a34be9abea54b806ef225fa5a27c3856a5fd224
walcark <kevin.walcarius@cnes.fr>
2026-04-02 10:19:34 +0200

    del: remove current test for SceneModuleSweep.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+"""Test whether the working environment has Dask or Cuda."""
 import pytest
 
 
@@ -7,4 +8,12 @@ try:
 except ImportError:
     _HAS_CUDA = False
 
+try:
+    import dask  # noqa: F401
+    _HAS_DASK = True
+except ImportError:
+    _HAS_DASK = False
+
+
+requires_dask = pytest.mark.skipif(not _HAS_DASK, reason="dask not available")
 requires_cuda = pytest.mark.skipif(not _HAS_CUDA, reason="CUDA not available")
