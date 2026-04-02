4b77efe50198392a1075e8f8b3183f51e07bfbb3
walcark <kevin.walcarius@cnes.fr>
2026-04-02 10:42:01 +0200

    fix: add create_atmosphere the atmosphere module.

--- src/adjeff/atmosphere/__init__.py
+++ src/adjeff/atmosphere/__init__.py
@@ -1,11 +1,13 @@
 """Define all classes and operations related to the Atmosphere."""
 
 from .atmo_config import AtmoConfig
+from .atmo_factory import create_atmosphere
 from .geo_config import GeoConfig
 from .spectral_config import SpectralConfig
 
 __all__ = [
     "AtmoConfig",
+    "create_atmosphere",
     "GeoConfig",
     "SpectralConfig",
 ]
