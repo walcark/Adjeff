e84f3bcf7a732aa0db68f0486b99eb17083287c0
walcark <kevin.walcarius@cnes.fr>
2026-04-17 11:29:22 +0200

    feat: add imports to the  module.

--- src/adjeff/modules/__init__.py
+++ src/adjeff/modules/__init__.py
@@ -3,11 +3,13 @@
 from .pipeline import Pipeline
 from .scene_module import SceneModule, TrainableSceneModule
 from .scene_module_sweep import SceneModuleSweep
+from .scene_source import SceneSource
 from .test_module import TestModule
 
 __all__ = [
     "Pipeline",
     "SceneModule",
+    "SceneSource",
     "TrainableSceneModule",
     "SceneModuleSweep",
     "TestModule",
--- src/adjeff/modules/samplers/__init__.py
+++ src/adjeff/modules/samplers/__init__.py
@@ -3,6 +3,7 @@
 from .psf_atm import SmartgSampler_PSF_Atm
 from .radiatives import RadiativePipeline
 from .rho_atm import SmartgSampler_Rho_atm
+from .rho_toa import SmartgSampler_Rho_toa
 from .rho_toa_sym import SmartgSampler_Rho_toa_sym
 from .sph_alb import SmartgSampler_Sph_alb
 from .tdif_down import SmartgSampler_Tdif_down
@@ -14,6 +15,7 @@ __all__ = [
     "SmartgSampler_PSF_Atm",
     "RadiativePipeline",
     "SmartgSampler_Rho_atm",
+    "SmartgSampler_Rho_toa",
     "SmartgSampler_Rho_toa_sym",
     "SmartgSampler_Sph_alb",
     "SmartgSampler_Tdif_down",
